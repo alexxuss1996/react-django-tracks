a05df983d303e2921ec7be119c36fa114f52c1b0
alexxuss1996 <alekseymelnych132@gmail.com>
2019-06-05 19:57:37 +0300

    Create Users

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,11 +1,15 @@
 import graphene
 import tracks.schema
+import users.schema
+
 
 class Query(tracks.schema.Query, graphene.ObjectType):
-	pass
+    pass
 
-class Mutation(tracks.schema.Mutation, graphene.ObjectType):
-	pass
+
+class Mutation(users.schema.Mutation, tracks.schema.Mutation, graphene.ObjectType):
+    pass
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
